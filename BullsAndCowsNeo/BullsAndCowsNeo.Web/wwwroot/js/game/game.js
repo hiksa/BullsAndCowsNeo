c6de88a53fed780cda7e5cd733180fe8b4a8fdea
Nikola Ivanov <nikola4421@gmail.com>
2018-06-04 12:18:36 +0300

    Adding initial phaser setup

---
+++ BullsAndCowsNeo/BullsAndCowsNeo.Web/wwwroot/js/game/game.js
@@ -0,0 +1,7 @@
+﻿let game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+
+game.state.add('titleState', titleState);
+game.state.add('queueState', queueState);
+game.state.add('gameState', gameState);
+
+game.state.start('titleState');
