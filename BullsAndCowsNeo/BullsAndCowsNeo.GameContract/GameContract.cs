415cdf3a4129e5b5733b2a19ced8d0fac4d652ff
Zdravko Georgiev <skinsomnia@gmail.com>
2018-06-24 21:44:32 +0300

    Game contract added

---
+++ BullsAndCowsNeo/BullsAndCowsNeo.GameContract/GameContract.cs
@@ -0,0 +1,64 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace BullsAndCowsNeo.GameContract
+{
+    public class GameContract : SmartContract
+    {
+        public static readonly byte[] ContractOwner = "AcKA1A3TRx6ubNzi3Dz2QFW6V9uEkeVasg".ToScriptHash();
+
+        public static object Main(string operation, params object[] args)
+        {
+            if (Runtime.Trigger == TriggerType.Verification)
+            {
+                if (ContractOwner.Length == 20)
+                {
+                    return Runtime.CheckWitness(ContractOwner);
+                }
+                else if (ContractOwner.Length == 33)
+                {
+                    byte[] signature = operation.AsByteArray();
+                    return SmartContract.VerifySignature(signature, ContractOwner);
+                }
+            }
+            else if (Runtime.Trigger == TriggerType.VerificationR)
+            {
+                return true;
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "join")
+                {
+                    if (args.Length != 2) return false;
+
+                    var gameId = (string)args[0];
+                    var address = (byte[])args[1];
+
+                    return GameLogic.JoinGame(gameId.AsByteArray(), address);
+                }
+                else if (operation == "pick")
+                {
+                    if (args.Length != 3) return false;
+
+                    var gameId = (string)args[0];
+                    var address = (byte[])args[1];
+                    var number = (string)args[2];
+                    return GameLogic.PickNumber(gameId.AsByteArray(), address, number);
+                }
+                else if (operation == "guess")
+                {
+                    if (args.Length != 3) return false;
+
+                    var gameId = (string)args[0];
+                    var address = (byte[])args[1];
+                    var number = (string)args[2];
+                    return GameLogic.GuessNumber(gameId.AsByteArray(), address, number);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ BullsAndCowsNeo/BullsAndCowsNeo.GameContract/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BullsAndCowsNeo.GameContract.Models
+{
+    [Serializable]
+    public class Game
+    {
+        public byte[] Id;
+
+        public byte[] FirstPlayer;
+
+        public byte[] SecondPlayer;
+
+        public string FirstPlayerNumber;
+
+        public string SecondPlayerNumber;
+
+        public GameState State;
+    }
+}
